6b843d3a878398e0ebdd02a68209a6f23072a9c9
Xkiller <med.ennahas@gmail.com>
2025-02-03 15:54:27 +0000

    old tracking

--- main.py
+++ main.py
@@ -74,78 +74,39 @@ def save_to_gcs(data, file_name):
 @app.route('/api/<path:random_characters>', methods=['GET'])
 def show_image_nometadata(random_characters):
     try:
-        number = random_characters.split('-')[-1]
-        number = int(number)  # Ensure it's a valid number
-        if number:
-            # Path to the image file
-            file_path = f'static/{number}.png'
+        # Path to the image file
+        file_path = f'static/1497.png'
 
-            # Check if the file exists
-            if not os.path.exists(file_path):
-                return jsonify({'error': 'Image not found'}), 404
+        # Check if the file exists
+        if not os.path.exists(file_path):
+            return jsonify({'error': 'Image not found'}), 404
 
-            # Generate random metadata
-            random_title = faker.sentence()
-            random_description = faker.paragraph()
-            random_keywords = ", ".join(faker.words(5))  # 5 random keywords
+        # Generate random metadata
+        random_title = faker.sentence()
+        random_description = faker.paragraph()
+        random_keywords = ", ".join(faker.words(5))  # 5 random keywords
 
-            # Read the image file
-            with open(file_path, 'rb') as img_file:
-                img_data = img_file.read()
+        # Read the image file
+        with open(file_path, 'rb') as img_file:
+            img_data = img_file.read()
 
-            # Generate random cookies
-            random_cookie1 = faker.word()
-            random_cookie2 = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        # Generate random cookies
+        random_cookie1 = faker.word()
+        random_cookie2 = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
-            # Create the response
-            response = make_response(img_data)
-            response.headers['Content-Type'] = 'image/png'
-            response.headers['X-Image-Title'] = random_title
-            response.headers['X-Image-Description'] = random_description
-            response.headers['X-Image-Keywords'] = random_keywords
-
-            # Set cookies with random values
-            response.set_cookie('__cf_bm_' + ''.join(random.choices(string.ascii_letters, k=3)), random_cookie1,
+        # Create the response
+        response = make_response(img_data)
+        response.headers['Content-Type'] = 'image/png'
+        response.headers['X-Image-Title'] = random_title
+        response.headers['X-Image-Description'] = random_description
+        response.headers['X-Image-Keywords'] = random_keywords
+        # Set cookies with random values
+        response.set_cookie('__cf_bm_' + ''.join(random.choices(string.ascii_letters, k=3)), random_cookie1,
                                 max_age=60 * 60)
-            response.set_cookie('_umsid_' + ''.join(random.choices(string.ascii_letters, k=4)), random_cookie2,
-                                max_age=60 * 60)
+        response.set_cookie('_umsid_' + ''.join(random.choices(string.ascii_letters, k=4)), random_cookie2,
+                            max_age=60 * 60)
+        return response
 
-            return response
-        else:
-            # Path to the image file
-            file_path = f'static/1497.png'
-
-            # Check if the file exists
-            if not os.path.exists(file_path):
-                return jsonify({'error': 'Image not found'}), 404
-
-            # Generate random metadata
-            random_title = faker.sentence()
-            random_description = faker.paragraph()
-            random_keywords = ", ".join(faker.words(5))  # 5 random keywords
-
-            # Read the image file
-            with open(file_path, 'rb') as img_file:
-                img_data = img_file.read()
-
-            # Generate random cookies
-            random_cookie1 = faker.word()
-            random_cookie2 = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-
-            # Create the response
-            response = make_response(img_data)
-            response.headers['Content-Type'] = 'image/png'
-            response.headers['X-Image-Title'] = random_title
-            response.headers['X-Image-Description'] = random_description
-            response.headers['X-Image-Keywords'] = random_keywords
-
-            # Set cookies with random values
-            response.set_cookie('__cf_bm_' + ''.join(random.choices(string.ascii_letters, k=3)), random_cookie1,
-                                max_age=60 * 60)
-            response.set_cookie('_umsid_' + ''.join(random.choices(string.ascii_letters, k=4)), random_cookie2,
-                                max_age=60 * 60)
-
-            return response
     except ValueError:
         return "nothing"
 
