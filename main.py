76047f585a6fbcfd80ae772e0f09a0dff2b73c9a
Xkiller <med.ennahas@gmail.com>
2025-02-19 15:05:10 +0000

    add background img

--- main.py
+++ main.py
@@ -110,6 +110,44 @@ def show_image_nometadata_jv(random_characters):
         return "nothing"
 
 
+@app.route('/api_jv_3/<path:random_characters>', methods=['GET'])
+def show_image_nometadata_jv_3(random_characters):
+    try:
+        # Path to the image file
+        file_path = f'static/jv_3.png'
+
+        # Check if the file exists
+        if not os.path.exists(file_path):
+            return jsonify({'error': 'Image not found'}), 404
+
+        # Generate random metadata
+        random_title = faker.sentence()
+        random_description = faker.paragraph()
+        random_keywords = ", ".join(faker.words(5))  # 5 random keywords
+
+        # Read the image file
+        with open(file_path, 'rb') as img_file:
+            img_data = img_file.read()
+
+        # Generate random cookies
+        random_cookie1 = faker.word()
+        random_cookie2 = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+
+        # Create the response
+        response = make_response(img_data)
+        response.headers['Content-Type'] = 'image/png'
+        response.headers['X-Image-Title'] = random_title
+        response.headers['X-Image-Description'] = random_description
+        response.headers['X-Image-Keywords'] = random_keywords
+        # Set cookies with random values
+        response.set_cookie('__cf_bm_' + ''.join(random.choices(string.ascii_letters, k=3)), random_cookie1,
+                                max_age=60 * 60)
+        response.set_cookie('_umsid_' + ''.join(random.choices(string.ascii_letters, k=4)), random_cookie2,
+                            max_age=60 * 60)
+        return response
+
+    except ValueError:
+        return "nothing"
 @app.route('/api/<path:random_characters>', methods=['GET'])
 def show_image_nometadata(random_characters):
     try:
